6bd8721cea863c26f6f99b55cac2b677b8917282
Deepika Mohan <deepikamm12@gmail.com>
2018-07-30 12:17:37 +0530

    files have been added

---
+++ MVC Application/Controllers/TicketController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCApplication.Models;
+
+namespace MVC_Application.Controllers
+{
+    public class TicketController : Controller
+    {
+        public IActionResult Index()
+        {
+            var s = new Ticket() { Location = "deepika", MyProperty = "deepika" };
+            return View(s);
+        }
+        public string Index2()
+        {
+            return "hello";
+        }
+    }
+}
---
+++ MVC Application/Models/Ticket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApplication.Models
+{
+    public class Ticket
+    {
+    public string  Location { get; set; }
+    public string MyProperty { get; set; }
+    
+}
+}
